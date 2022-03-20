b639267dfea8d227cc990d00124542a2696ebec8
Hernán D. Mercado <hernandmf@gmail.com>
2022-03-20 12:17:12 -0500

    creating the context to the application

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,28 @@
 import { Container, Grid, Typography } from "@mui/material";
 import Formulario from "./components/Formulario";
+import { NoticiasProvider } from "./context/NoticiasProvider";
 
 function App() {
   return (
-    <Container>
-      <header>
-        <Typography align="center" marginY={5} component="h1" variant="h3">
-          Buscador de noticias
-        </Typography>
-      </header>
-      <Grid
-        container
-        direction="row"
-        justifyContent="center"
-        alignItems="center"
-      >
-        <Grid item xs={12} md={6} lg={4}>
-          <Formulario />
+    <NoticiasProvider>
+      <Container>
+        <header>
+          <Typography align="center" marginY={5} component="h1" variant="h3">
+            Buscador de noticias
+          </Typography>
+        </header>
+        <Grid
+          container
+          direction="row"
+          justifyContent="center"
+          alignItems="center"
+        >
+          <Grid item xs={12} md={6} lg={4}>
+            <Formulario />
+          </Grid>
         </Grid>
-      </Grid>
-    </Container>
+      </Container>
+    </NoticiasProvider>
   );
 }
 
